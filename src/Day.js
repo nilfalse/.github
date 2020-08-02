26ae476e5674c4e6c3c6a6371d3ec308d64d59a7
Yaroslav Ilin <nilfalse@yandex.com>
2020-08-02 18:27:51 +0200

    add outline

--- src/Day.js
+++ src/Day.js
@@ -4,9 +4,24 @@ import { dpi } from './config';
 
 const shades = ['#ebedf0', '#9be9a8', '#40c463', '#30a14e', '#216e39'];
 
+const style = {
+  shapeRendering: 'geometricPrecision',
+  outline: `${1 * dpi}px solid rgba(27,31,35,.04)`,
+  outlineOffset: `${-1 * dpi}px`,
+};
+
 export function Day({ x, num, density }) {
   const y = num * 13 * dpi;
   const fill = shades[density];
 
-  return <rect width={10 * dpi} height={10 * dpi} x={x} y={y} fill={fill} />;
+  return (
+    <rect
+      width={10 * dpi}
+      height={10 * dpi}
+      x={x}
+      y={y}
+      style={style}
+      fill={fill}
+    />
+  );
 }
